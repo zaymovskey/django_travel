2e6b8fbbd0cb488056319d36c0a28407cc51bf37
Zaymovskey <zaymovskey@mail.ru>
2020-12-21 11:58:26 +0300

    Добавлен base.html

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = '(r1*pkv0@002m!^vt5k530oqc)bv^y3z%^o9^kr9iij*=_luvu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tours'
 ]
 
 MIDDLEWARE = [
--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_view(request):
+    return render(request, 'tours/index.html')
+
+
+def departure_view(request):
+    return render(request, 'tours/index.html')
+
+
+def tour_view(request):
+    return render(request, 'tours/index.html')
